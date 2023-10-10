76c7d844a27089c351217d812261137275226a2d
George Amuzu <gnamuzu@gmail.com>
2023-10-10 10:10:42 +0100

    updated

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
+import { useState } from 'react';
 import Outpost from './outpost/outpost'
 import Checkout from './checkout/Checkout'
-import ProductDetails from './products/ProductDetails';
-import Header from './header/Header';
+import ProductDetails from './products/ProductDetails'
+import Header from './header/Header'
 
 function App() {
+  const [cart, setCart] = useState([])
+  
   return (
     <BrowserRouter>
       <Header />
       <Routes>
-        <Route path='/outpost' element={<Outpost />} />
-        <Route path='/checkout' element={<Checkout />} />
+        <Route path='/outpost' element={<Outpost cart={cart} setCart={setCart} />} />
+        <Route path='/checkout' element={<Checkout cart={cart} />} />
         <Route path='/product-details/:slug' element={<ProductDetails />} />
         <Route path='/' element={<Navigate to='/outpost' />} />
       </Routes>
