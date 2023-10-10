3211a3fbc40ffe24b40af35ee0e942bc6adcd05b
George Amuzu <gnamuzu@gmail.com>
2023-10-10 08:22:32 +0100

    updated product listing

--- src/products/ProductListing.js
+++ src/products/ProductListing.js
@@ -1,16 +1,36 @@
+import { useSearchParams } from 'react-router-dom'
+
 import products from '../products/products.json'
 import styles from './ProductListing.module.css'
 import Product from './Product'
 
 function ProductListing(props) {
+    const [searchParams, setSearchParams] = useSearchParams()
+    const filter = searchParams.get('filter')
+
+    function setFilter(filter) {
+        setSearchParams({filter})
+    }
+
     return (
+        <>
+        <div className={styles.filters}>
+            <button className='button' onClick={() => setFilter('transport')}> Transport </button>
+            <button className='button' onClick={() => setFilter('weapons')}> Weapons </button>
+            <button className='button' onClick={() => setFilter('defense')}> Defense </button>
+            <button className='button' onClick={() => setFilter('Equipment')}> Equipment </button>
+            <button className='button' onClick={() => setFilter('')}> All </button>
+        </div>
         <ul className={styles.products}>
-            {products.map((product, idx) => (
+            {products
+            .filter(p => !filter || p.category === filter)
+            .map((product, idx) => (
                 <li key={idx}>
                     <Product productData = {product} addToCart={props.addToCart} />
                 </li>)
             )}
         </ul>
+        </>
     )
 }
 
